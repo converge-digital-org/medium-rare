a929a16fcd54b09056447502d5e9bbcdc5ff2df7
converge-digital-org <kyle@converge.digital>
2024-12-13 15:45:01 -0800

    Update si_the_party_app.js

--- si_the_party_app.js
+++ si_the_party_app.js
@@ -205,11 +205,11 @@ function initializeFormEventListener() {
             event.preventDefault();
 
             const formData = {
-                first_name: document.querySelector("#name-yui_3_17_2_1_1733252193375_12106-fname-field")?.value || null,
-                last_name: document.querySelector("#name-yui_3_17_2_1_1733252193375_12106-lname-field")?.value || null,
-                email: document.querySelector("#email-yui_3_17_2_1_1733252193375_12107-field")?.value || null,
-                phone_country: document.querySelector("#phone-c48ec3b8-6c62-4462-aa21-af587054f3ef-country-code-field")?.value || null,
-                phone_number: document.querySelector("#phone-c48ec3b8-6c62-4462-aa21-af587054f3ef-input-field")?.value || null
+                first_name: document.querySelector("#name-yui_3_17_2_1_1733853576725_5390-fname-field")?.value || null,
+                last_name: document.querySelector("#name-yui_3_17_2_1_1733853576725_5390-lname-field")?.value || null,
+                email: document.querySelector("#email-yui_3_17_2_1_1733853576725_5391-field")?.value || null,
+                phone_country: document.querySelector("#phone-eccc1f69-8612-4538-a65e-6658099734f6-country-code-field")?.value || null,
+                phone_number: document.querySelector("#phone-eccc1f69-8612-4538-a65e-6658099734f6-input-field")?.value || null
             };
 
             const additionalParams = await getAdditionalParams();
